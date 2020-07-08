806462b2963d5eabf7452c0765db131250fd60c5
Alexa Kelly <kellyav@dukes.jmu.edu>
2020-07-08 18:30:23 -0400

    Update films.sql

--- films.sql
+++ films.sql
@@ -13,37 +13,45 @@ WHERE release_year = 2016;
 SELECT *
 FROM films
 WHERE language= 'French’;
+
 -- Get all details for Spanish language films released after 2000, but before 2010.
 SELECT * 
 FROM films
 WHERE language='Spanish' AND release_year> 2000 AND release_year <2010;
+
 /* Lets get more specific… 
 Get the title and release year for films released in the 90s, 
 filters the records to only include French or Spanish language films, 
 and only return films that took in more than $2M gross. */
+
 SELECT title, release_year
 FROM films
 WHERE (release_year >= 1990 AND release_year < 2000) AND (language= 'French' OR language= 'Spanish')
 AND gross > 2000000;
+
 -- Get the title and release year of all films released between 1990 and 2000 (inclusive), select only films that have budgets over $100 million, only return Spanish or French language films.
 SELECT title, release_year 
 FROM films
 WHERE (release_year BETWEEN 1990 AND 2000)
 AND budget > 100000000 
 AND (language= 'Spanish' OR language= 'French');
+
 -- Get the title and release year of all films released in 1990 or 2000 that were longer than two hours.
 SELECT title, release_year
 FROM films
 WHERE release_year IN (1990, 2000)
 AND duration > 120;
+
 --Get the title and language of all films which were in English, Spanish, or French.
 SELECT title, language
 FROM films
 WHERE language IN ('English', 'Spanish', 'French');
+
 --Get the title and certification of all films with an NC-17 or R certification.
 SELECT title, certification
 FROM films
 WHERE certification IN ('R', 'NC-17')
+
 --Get the title of every film which doesn't have a budget associated with it.
 SELECT title 
 FROM films

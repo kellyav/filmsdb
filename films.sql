533b26cbb533b45d3eb74027ff737537c2901bb8
Alexa Kelly <kellyav@dukes.jmu.edu>
2020-07-14 20:17:09 -0400

    Update films.sql

--- films.sql
+++ films.sql
@@ -14,7 +14,7 @@ SELECT *
 FROM films
 WHERE language= 'French’;
 
--- Get all details for Spanish language films released after 2000, but before 2010.
+-- get all details for Spanish language films released after 2000, but before 2010.
 SELECT * 
 FROM films
 WHERE language='Spanish' AND release_year> 2000 AND release_year <2010;

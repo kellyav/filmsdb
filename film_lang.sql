01ad9f68acf5a48616be42c7be01f53d7a860233
Alexa Kelly <kellyav@dukes.jmu.edu>
2020-07-15 21:55:49 -0400

    Update film_lang.sql

--- film_lang.sql
+++ film_lang.sql
@@ -8,20 +8,20 @@ FROM films
 WHERE language= 'French’;
 
 
---Get the number of films which don't have a language associated with them.
+--number of films which don't have a language associated with them.
 SELECT COUNT(*)
 FROM films
 WHERE language IS NULL;
 
 
---Get the title and language of all films which were in English, Spanish, or French.
+--title and language of all films which were in English, Spanish, or French.
 SELECT title, language
 FROM films
 WHERE language IN ('English', 'Spanish', 'French');
 
 
 /* Lets get more specific… 
-Get the title and release year for films released in the 90s, 
+title and release year for films released in the 90s, 
 filters the records to only include French or Spanish language films, 
 and only return films that took in more than $2M gross. */
 
@@ -30,7 +30,7 @@ FROM films
 WHERE (release_year >= 1990 AND release_year < 2000) AND (language= 'French' OR language= 'Spanish')
 AND gross > 2000000;
 
-/* Get the title and release year of all Spanish or French language films that: 
+/* title and release year of all Spanish or French language films that: 
 were released between 1990 and 2000 (inclusive), and with budgets over $100 million */
 SELECT title, release_year 
 FROM films
